de1384a8be4db3a24c71d6a46f981a937267437e
Flor Loustaunau <florloustaunau@gmail.com>
2020-09-26 10:31:58 -0300

    primer commit de Mascota

---
+++ Mascota.java
@@ -0,0 +1,15 @@
+public class Mascota {
+	private String nombre;
+
+	public Mascota(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ PruebaMascota.java
@@ -0,0 +1,7 @@
+public class PruebaMascota {
+
+	public static void main (String[] args) {
+		Mascota m = new Mascota ("Tiburio");
+		System.out.println("El nombre de la mascota es "+ m.getNombre());
+	}
+}
