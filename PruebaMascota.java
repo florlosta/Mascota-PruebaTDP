7f38416cf4a10a7b23895ce867dc3911da414ad2
Flor Loustaunau <florloustaunau@gmail.com>
2020-09-26 10:38:31 -0300

    commit modificiacion PruebaMascota.java

--- PruebaMascota.java
+++ PruebaMascota.java
@@ -1,7 +1,7 @@
 public class PruebaMascota {
 
 	public static void main (String[] args) {
-		Mascota m = new Mascota ("Tiburio");
+		Mascota m = new Mascota ("Catrula");
 		System.out.println("El nombre de la mascota es "+ m.getNombre());
 	}
 }
